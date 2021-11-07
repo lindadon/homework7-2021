5d4f43d80d24f81b87f11672693ca9ff2a8fabd5
lindadon <70586810+lindadon@users.noreply.github.com>
2021-11-07 17:09:34 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -20,7 +20,7 @@ console.log("Pause Video");
     });
 
 document.querySelector("#slower").addEventListener("click", function SlowVideo() {
-video.playbackRate=video.playbackRate * 0.05;
+video.playbackRate=video.playbackRate * 0.95;
 	console.log("Slowed video speed is " + video.playbackRate)
     });
 
@@ -30,11 +30,14 @@ video.playbackRate=video.playbackRate * 1.05;
 	});
 
 document.querySelector("#skip").addEventListener("click", function() {
-video.currentTime += 15;
+	console.log("Original location " + video.currentTime)
+	video.currentTime += 15;
+	console.log("New location " + video.currentTime)
 	if(video.currentTime > video.duration - 15) {
 		video.currentTime=0
+		console.log("Going back to beginning")
 	}
-   console.log("Skipped to" + video.currentTime);
+
     });
 
 mutebutton = document.querySelector("#mute")
@@ -52,11 +55,11 @@ document.querySelector("#mute").addEventListener("click", function() {
 
 var slider=document.querySelector("#slider");
 var output=document.querySelector("#volume")
-output.innerHTML = slider.value
+output.innerHTML = slider.value + "%";
 
 document.querySelector("#slider").addEventListener("click", function() {
-	output.innerHTML = this.value;
-	video.volume = slider.value
+	output.innerHTML = this.value + "%";
+	video.volume = slider.value / 100
      console.log("Slider Video Volume is " + this.value);
      });
 
