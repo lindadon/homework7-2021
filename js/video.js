3ebf2edba8c4795634eb073c7b45cb68bcbe85c9
lindadon <70586810+lindadon@users.noreply.github.com>
2021-11-07 17:29:01 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -37,7 +37,7 @@ document.querySelector("#skip").addEventListener("click", function() {
 	console.log("Original location " + video.currentTime)
 	video.currentTime += 15;
 	console.log("New location " + video.currentTime)
-	if(video.currentTime >= video.duration - 15) {
+	if(video.currentTime > video.duration - 15) {
 		video.currentTime=0
 		video.play()
 		console.log("Going back to beginning")
